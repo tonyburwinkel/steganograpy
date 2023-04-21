a4fc46f9bfccd8b955d710e84c96e2e4c35d41a5
tonyburwinkel <tony.burwinkel@gmail.com>
2023-04-21 18:43:16 -0400

    polished gui. added decode button, exit. TODO: view 2 images side by side

--- gui/menu.py
+++ gui/menu.py
@@ -1,59 +1,12 @@
 from tkinter import *
+import tkinter as tk
 import os
-from PIL import Image, ImageTk
+from PIL import Image
+from PIL import ImageTk
   
 # Create object
-root = Tk()
   
-# Change the label text
-def show():
-    label.config( text = selected.get() )
-
-def load_image():
-	current = Image.open(selected.get())
-	new_photo = ImageTk.PhotoImage(Image.open(f"{selected.get()}"))
-	print(selected.get())
-	label.config(image = new_photo)
-	label.image=new_photo
-
-# Dropdown menu options
-options = list(filter(lambda x: x[-3:]=="ppm", os.listdir()))
-  
-# datatype of menu text (tkinter.StringVar())
-selected = StringVar()
-# initial menu text
-selected.set(f"{options[0]}" )
-  
-# Create Dropdown menu
-drop = OptionMenu( root , selected , *options )
-drop.pack()
-	
-#label = Label(root, image=photo)
-#label.pack()
-  
-# Create button (its window, its text, its event listener)
-button = Button( root , text = "load" , command = load_image )
-button.pack() 
-
-# load an image to start with
-label_init = ImageTk.PhotoImage(Image.open(f"{options[0]}"))
-# Create Label
-label = Label( root , image = label_init )
-label.pack()
-
-stego_text = Text(root, height = 5, width = 52)
-stego_text.pack()
-
-save_name = Text(root, height = 1, width = 40)
-save_name.pack()
-
-'''
-event handler for encode button
-gets the text from stego_text and save_name
-do what you want with them
-clear the boxes
-'''
-def encode_image():
+def mk_txt():
 	print("encoding image!")
 	text = stego_text.get("1.0", "end-1c")
 	with open(f"{save_name.get('1.0', 'end-1c')}", "w") as f:
@@ -63,8 +16,81 @@ def encode_image():
 
 	print(text)
 
-encode = Button( root , text = "encode", command = encode_image )
-encode.pack() 
+def decode_image():
+	from image_decoder import ImageDecoder
+	encoder = ImageDecoder(selected.get())
+	message = encoder.msg
+	stego_text.insert("1.0", message)
+	print(message)
+
+def encode_image():
+	text = stego_text.get("1.0", "end-1c")
+	stego_text.delete("1.0","end")
+	name = save_name.get("1.0", "end-1c")
+	save_name.delete("1.0","end")
+	from image_encoder import ImageEncoder
+	encoder = ImageEncoder(text,  f"{selected.get()}")
+	encoder.create_steg_img()
+	encoder.save_steg_image(f"{name}.ppm")
+	refresh_drop()
+
+def refresh_drop():
+	# Reset var and delete all old options
+	selected.set('')
+	drop['menu'].delete(0, 'end')
+
+	# Insert list of new options (tk._setit hooks them up to var)
+	new_choices = list(filter(lambda x: x[-3:]=="ppm", os.listdir()))
+	for choice in new_choices:
+		drop['menu'].add_command(label=choice, command=tk._setit(selected, choice))
+	selected.set("choose image")
+
+def load_image():
+	current = Image.open(selected.get())
+	new_photo = ImageTk.PhotoImage(Image.open(f"{selected.get()}"))
+	print(selected.get())
+	picture.config(image = new_photo)
+	picture.image=new_photo
+
+def exit_app():
+	root.destroy()
+	exit(1)
+
+root = Tk()
+
+# make a list of  options to fill the drop menu with
+options = list(filter(lambda x: x[-3:]=="ppm", os.listdir()))
+  
+# datatype of menu text (tkinter.StringVar())
+selected = StringVar()
+# initial menu text
+selected.set(f"{options[0]}" )
+  
+# Create Dropdown menu
+drop = OptionMenu( root , selected , *options )
+	
+# Create load button (its parent, its text, its event listener)
+load_btn = Button( root , text = "load" , command = load_image )
+encode_btn = Button( root , text = "encode", command = encode_image )
+decode_btn = Button( root , text = "decode", command = decode_image )
+exit_button = Button(root, text="Exit", command=exit_app)
+
+# load an image to start with
+label_init = ImageTk.PhotoImage(Image.open(f"{options[0]}"))
+# Create Label with initial image to load
+picture = Label( root , image = label_init )
+
+stego_text = Text(root, height = 5, width = 52)
+save_name = Text(root, height = 1, width = 40)
+
+drop.pack()
+load_btn.pack(pady=10) 
+picture.pack()
+stego_text.pack(pady=10)
+save_name.pack()
+encode_btn.pack(pady=10) 
+decode_btn.pack()
+exit_button.pack(pady=20)
   
 # Execute tkinter
 root.mainloop()
