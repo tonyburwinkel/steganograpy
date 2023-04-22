2a04357e506ca7878c2cef11963559f08258b917
tonyburwinkel <tony.burwinkel@gmail.com>
2023-04-21 23:11:28 -0400

    more gui adjustment

--- gui/stego_py.py
+++ gui/stego_py.py
@@ -12,16 +12,16 @@ def decode_image():
 	message = encoder.msg
 	stego_text.delete("1.0", "end")
 	stego_text.insert("1.0", message)
-	print(message)
 
 def encode_image():
 	text = stego_text.get("1.0", "end-1c")
 	name = save_name.get("1.0", "end-1c")
-	#clear_text()
+	print(name)
 	from image_encoder import ImageEncoder
 	encoder = ImageEncoder(text,  f"{selected.get()}")
 	encoder.create_steg_img()
 	encoder.save_steg_image(f"{name}.ppm")
+	clear_text()
 	refresh_drop()
 
 def refresh_drop():
@@ -44,6 +44,7 @@ def load_image():
 	clear_text()
 
 def clear_text():
+	print("clear")
 	stego_text.delete("1.0","end")
 	save_name.delete("1.0","end")
 
@@ -71,13 +72,16 @@ decode_btn = Button( root , text = "decode", command = decode_image )
 exit_button = Button(root, text="Exit", command=exit_app)
 
 # load an image to start with
-label_init = ImageTk.PhotoImage(Image.open(f"{options[0]}"))
+photo_init = ImageTk.PhotoImage(Image.open(f"{options[0]}"))
 # Create Label with initial image to load
-picture = Label( root , image = label_init )
+picture = Label( root , image = photo_init )
 
-stego_text = Text(root, height = 5, width = 52)
-save_name = Text(root, height = 1, width = 40)
+stego_text = Text(root, height = 5)
 
+save_label = Label(root, text="encode as:")
+save_name = Text(root, height = 1)
+
+'''
 drop.pack()
 load_btn.pack(pady=10) 
 picture.pack()
@@ -86,6 +90,17 @@ save_name.pack()
 encode_btn.pack(pady=10) 
 decode_btn.pack()
 exit_button.pack(pady=20)
-  
+'''
+
+drop.grid(row=0, column=0, columnspan=2, sticky="ew", padx=10, pady=10)
+load_btn.grid(row=0, column=2, sticky="ew") 
+picture.grid(row=1, column=0, columnspan=3, pady=10)
+stego_text.grid(row=2, column=0, columnspan=3, sticky="ew", padx=10)
+save_label.grid(row=3, column=0)
+save_name.grid(row=3, column=1, columnspan=2)
+encode_btn.grid(row=4, column=0, sticky="ew") 
+decode_btn.grid(row=4, column=1, sticky="ew")
+exit_button.grid(row=4, column=2, sticky="ew")
+
 # Execute tkinter
 root.mainloop()
